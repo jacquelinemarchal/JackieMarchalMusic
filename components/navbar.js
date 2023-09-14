637b55057bf33d9ae20c86aec44ee247b6bfe5ac
jacquelinemarchal <64337067+jacquelinemarchal@users.noreply.github.com>
2023-09-14 11:51:25 -0400

    add sub-menu for listen tab

--- components/navbar.js
+++ components/navbar.js
@@ -24,10 +24,33 @@ navBarTemplate.innerHTML =
 
         nav a{
             color: #D6D9B6;
-            padding: 12px 16px;
             text-decoration: none;
             font-size: 22px;
             font-family: "Judson", serif;
+            padding-right: 4px;
+        }
+
+        nav li{
+            padding: 12px 16px;
+        }
+
+        #dropdownMenu a {
+            padding: 0;
+            font-size: 15px;
+        }
+
+        #dropdownMenu li {
+            padding: 8px 10px;
+            font-size: 15px;
+        }
+
+        #dropdownMenu{
+            position: absolute;
+            padding: 0;
+            background-color: #000000;
+            opacity: 0.7;
+            width: 170px;
+            border-radius: 2px;
         }
 
         #hamburger{
@@ -59,7 +82,29 @@ navBarTemplate.innerHTML =
         <div id="webNavBar">
             <ul id="navUl">
                 <li>
-                    <a href="home.html">music</a>
+                    <a target="_blank" href="https://share.amuse.io/album/jackie-marchal-as-is">listen</a>
+                    <svg fill="#D6D9B6" xmlns="http://www.w3.org/2000/svg" height=".8em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+                    <ul id="dropdownMenu">
+                        <li>
+                            <a target="_blank" href="https://open.spotify.com/artist/0impl8NVq1NoLQssU18V1P?si=MXM3iY_8R2mOlsHIaUA1ZA">spotify</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://music.apple.com/us/artist/jackie-marchal/1495174236">apple music</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://jackiemarchal.bandcamp.com/">bandcamp</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://music.amazon.com/artists/B083Y1QSJG/jackie-marchal">amazon music</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://music.youtube.com/browse/MPREb_gmrrWD3dsyq">youtube music</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://www.deezer.com/album/454169445">deezer</a>
+                        </li>
+                        
+                    </ul>
                 </li>
                 <li>
                     <a href="">writing</a>
