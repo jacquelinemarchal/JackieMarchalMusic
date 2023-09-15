b97449a0c92fadd95776b10513fb803c7b5f3d0c
jacquelinemarchal <64337067+jacquelinemarchal@users.noreply.github.com>
2023-09-15 18:48:49 -0400

    rework without nav-component

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ var toggleHamburger = () => {
     }
 }
 
-var listen = document.getElementById("listen");
-listen.addEventListener("mouseover", (event) => {
+var webListen = document.getElementById("webListen");
+var webDropdown = document.getElementById("webDropdownMenu");
+
+webListen.addEventListener("mouseover", () => {
+    webDropdown.style.display = "block";
+});
+
+webListen.addEventListener("mouseleave", () => {
+    webDropdown.style.display = "none";
+});
+
+var mobileListen = document.getElementById("mobileListen");
+var mobileDropdown = document.getElementById("mobileDropdownMenu");
+
+mobileListen.addEventListener("click", () => {
     console.log("hi")
+    if (mobileDropdown.style.display == "block"){
+        mobileDropdown.style.display = "none";
+    }
+    else{
+        mobileDropdown.style.display = "block";
+    }
 });
