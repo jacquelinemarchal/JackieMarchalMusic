a9853699b11ee09a69da1c7873d55bd42dac8e6e
jacquelinemarchal <64337067+jacquelinemarchal@users.noreply.github.com>
2023-09-18 11:56:51 -0400

    adjust bio pg for mobile

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var toggleHamburger = () => {
     let list = document.getElementById("mobileUl");
+    var bioContainer = document.querySelector(".bioContainer");
     if (list.style.display == "none"){
         list.style.display = "inline";
+        bioContainer.style.marginTop = "220px";
     }
     else{
         list.style.display = "none";
+        bioContainer.style.marginTop = "150px";
     }
 }
 
@@ -23,7 +26,6 @@ var mobileListen = document.getElementById("mobileListen");
 var mobileDropdown = document.getElementById("mobileDropdownMenu");
 
 mobileListen.addEventListener("click", () => {
-    console.log("hi")
     if (mobileDropdown.style.display == "block"){
         mobileDropdown.style.display = "none";
     }
